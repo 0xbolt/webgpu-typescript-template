5822623d0e89013e45ede837378df492cbd6935d
0xbolt <0xpapercut@gmail.com>
2025-12-17 12:31:21 +0100

    Update utils

--- src/utils.ts
+++ src/utils.ts
@@ -9,3 +9,28 @@ export function addFullscreenEventListener(window: Window, element: HTMLElement)
         }
     });
 }
+
+export class CanvasResizeObserver {
+    observer: ResizeObserver;
+
+    constructor(device: GPUDevice, render: () => void) {
+        this.observer = new ResizeObserver(entries => {
+            for (const entry of entries) {
+                const width = entry.devicePixelContentBoxSize?.[0].inlineSize || entry.contentBoxSize[0].inlineSize * window.devicePixelRatio;
+                const height = entry.devicePixelContentBoxSize?.[0].blockSize || entry.contentBoxSize[0].blockSize * window.devicePixelRatio;
+                const canvas = entry.target as HTMLCanvasElement;
+                canvas.width =  Math.max(1, Math.min(width, device.limits.maxTextureDimension2D));
+                canvas.height =  Math.max(1, Math.min(height, device.limits.maxTextureDimension2D));
+            }
+            render();
+        });
+    }
+
+    observe(canvas: HTMLCanvasElement) {
+        try {
+            this.observer.observe(canvas, { box: 'device-pixel-content-box' });
+        } catch {
+            this.observer.observe(canvas, { box: 'content-box' });
+        }
+    }
+}
